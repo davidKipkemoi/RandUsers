201277c904d1f1739ac05a466462db66695a8b77
Kiprich <davidkipkemoi30@gmail.com>
2023-11-08 15:26:59 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Home from "./Components/Home"
-import './Components/header.css'
-import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
  
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import axios from 'axios'
 import { useState,useEffect } from 'react'
-import Paginator from './Paginator';
-
 export default function Home() {
     const [users,setUser]= useState([]);
     
@@ -18,36 +16,47 @@ export default function Home() {
             }
           }
           handleFetch()
-    },[])
-  
-          
+    },[])   
     const ListPeople = ({people})=>{
         return(
-            
-                <div  className='flex flex-wrap w-1/4 rounded-2xl bg-black'>
-                <img
-            src={people.picture.large}
-            className=''
-            />
-            <p className=' text-white'>Gender : {people.gender}</p>
-                </div>  
+         <div  className='flex w-1/4 rounded-2xl bg-white'>   
+            <div className='flex flex-col  md:flex-row'>
+              <img
+                  src={people.picture.large}
+                  className=''
+               />
+               <div className='flex flex-col'>
+                  <p className=' text-white'>Gender : {people.gender}</p>
+                    <p className=' text-white'>Country : {people.location.country}</p>
+                    <p className=' text-white'>City : {people.location.city}</p>
+                </div>
+                      </div>
+              </div>
+
         )  
     }
     
   return (
   
 <>
-<div className='flex flex-row w-full bg-black justify-center '>
+<div className='flex flex-row w-full '>
   <img
   className='h-28 w-40  mt-5 rounded-xl '
   src='https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1915&q=80'
   alt='description'
   />
-    <h2 className="header-top">Thee A Team Group</h2>
-
-
+    <div>
+      <div className="relative text-gray-600">
+    <input />
+    <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
+        <i className="fas fa-search"></i>
+    </button>
 </div>
-<div className='justify-center flex flex-wrap gap-6 bg-white   mt-5 h-max  '>
+    </div>
+</div>
+    
+
+<div className='justify-center flex flex-wrap gap-6 mt-5 h-max  '>
         {users.map((people,index)=>(
             <ListPeople
             key={index}
@@ -55,7 +64,6 @@ export default function Home() {
             />
             ))
         }
-        <Paginator/>
     </div>
 </>
     
